38412dccf51cbe7292bee32beaee01e7e90a0853
Caíque Maso <82621748+CaiqueBM@users.noreply.github.com>
2021-06-29 00:01:45 +0000

    Solicitando e digitando os valores que o usuário quer armazenar em uma struct

--- circuito.cpp
+++ circuito.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <locale.h>
+#include <fstream>
 
-struct tCircuito{
+struct tCircuito {
     float resistencia1;
     float resistencia2;
     int tensao;
+};
+
+struct tComandos {
+        int n = 0;
+    //ofstream enviar;
+    //ifstream receber;
+
+    //Gravar os dados no arquivo
+    //void gravar() {
+        
+    //}
+
 };
--- main.cpp
+++ main.cpp
@@ -5,22 +5,30 @@ using namespace std;
 
 int main() {
     int verificacao; //0 = gravar / 1 = ler
-    int N;
-    int valor;
+    //tCircuito valor2; //somente para gravar a quantidade de 
+    tComandos comando;
 
     cout << "Digite 0 para gravar e 1 para ler um arquivo: " << endl;
     cin >> verificacao;
 
     if(verificacao == 0) {
         cout << "Quantos dados deseja armazenar? " << endl;
-        cin >> N;
+        cin >> comando.n;
 
-        for (int i=0; i < N; i++) {
-            cout << "Item " << i + 1 << ":" << endl;
-            cin >> valor;
+        struct tCircuito valor[comando.n];
+
+        for (int i=0; i < comando.n ; i++) {
+            cout << "--------- Item " << i + 1 << "----------" << endl;
+            cout << "Resistência 1: ";
+            cin >> valor[i].resistencia1;
+            cout << "Resistência 2: ";
+            cin >> valor[i].resistencia2;
+            cout << "Fonte de tensão: ";
+            cin >> valor[i].tensao;
         }
         
     } else if (verificacao == 1) {
 
-    }
+        
+    } 
 }
